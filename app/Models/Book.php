1c56047a466cfdba3bdc5602649c1cd85856e89a
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-12-30 23:52:37 +0100

    defined review model and created a scope query

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Review;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -10,8 +11,14 @@ class Book extends Model
 {
     use HasFactory;
 
-    public function reviews()
+    public function reviews() //Laravel adds magic and this magic can be recall has a property of book: $book->reviews
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder // devo sempre mettere scope come prefix per incapsulare una query, mettere il builder e specificare il tipo di return
+    {
+
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 }
