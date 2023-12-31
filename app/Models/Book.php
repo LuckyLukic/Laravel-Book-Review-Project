95b916d705d9b3036bd35fcb185b804ea397c79a
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-12-31 14:38:52 +0100

    implemented query filters

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use App\Models\Review;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Book extends Model
 {
@@ -22,13 +23,38 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query): Builder
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withCount('rviews')->orderBy('reviews_count', 'desc')->get();
+        //fn as arrow function. only 1 expression allowed and no need to write call "use" to acess to outside variables
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderBy('reviews_count', 'desc');
+        //in this case withCount() add a new column to Book as reviews_count with the summ of the reviews associated to each single book)->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating')->get();
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
+
